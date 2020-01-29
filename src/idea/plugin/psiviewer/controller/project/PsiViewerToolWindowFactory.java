5c48f6cab265beb096bd15fa410752d4bbf045e0
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2020-01-29 16:52:45 +0100

    migrate to ToolWindowFactory

---
+++ src/idea/plugin/psiviewer/controller/project/PsiViewerToolWindowFactory.java
@@ -0,0 +1,27 @@
+package idea.plugin.psiviewer.controller.project;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowFactory;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
+import idea.plugin.psiviewer.controller.application.PsiViewerApplicationSettings;
+import idea.plugin.psiviewer.view.PsiViewerPanel;
+import org.jetbrains.annotations.NotNull;
+
+public class PsiViewerToolWindowFactory implements ToolWindowFactory {
+
+    @Override
+    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
+        PsiViewerProjectComponent component = PsiViewerProjectComponent.getInstance(project);
+        ContentManager contentManager = toolWindow.getContentManager();
+        PsiViewerPanel panel = component.initToolWindow(toolWindow);
+        Content content = contentManager.getFactory().createContent(panel, null, false);
+        contentManager.addContent(content);
+    }
+
+    @Override
+    public boolean shouldBeAvailable(@NotNull Project project) {
+        return PsiViewerApplicationSettings.getInstance().PLUGIN_ENABLED;
+    }
+}

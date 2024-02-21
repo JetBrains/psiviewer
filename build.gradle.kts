fb9c9f8b5610548e7a67defe734141691fd40cef
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2024-02-21 09:20:28 +0400

    Build 2025.1
    - java updated to 21
    - Gradle updated to 8.10
    - IntelliJ plugin updated to 2.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,25 +3,33 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 fun properties(key: String) = providers.gradleProperty(key)
 
 plugins {
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.6.0"
 }
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+        jetbrainsRuntime()
+        nightly()
+    }
 }
+
+val platformVersionProvider by extra(project.provider {
+    properties("platformVersion").get() + properties("platformBranch").get() + properties("platformBuild").get()
+})
 
 version = properties("pluginVersion").get().ifEmpty { properties("platformVersion").get() } +
         properties("pluginBranch").get().ifEmpty { properties("platformBranch").get() } +
         properties("pluginBuild").get().ifEmpty { properties("platformBuild").get() }
 
 apply(plugin = "java")
-apply(plugin = "org.jetbrains.intellij")
-intellij {
-    pluginName.set(properties("name").get())
-    version.set(project.provider {
-        properties("platformVersion").get() + properties("platformBranch").get() + properties("platformBuild").get()
-    })
-    updateSinceUntilBuild.set(true)
+apply(plugin = "org.jetbrains.intellij.platform")
+
+dependencies {
+    intellijPlatform {
+        create("IC", platformVersionProvider.get(), useInstaller = properties("useInstaller").get().toBoolean())
+    }
 }
 
 tasks {
